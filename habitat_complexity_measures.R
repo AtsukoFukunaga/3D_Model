994cd9752a1b30c6c2bcdce8670264e39d55d7d6
AtsukoFukunaga <atsuko.fukunaga@hawaii.edu>
2018-09-07 20:31:52 -1000

    Updating terrain_fun for d0 row/col == 1

--- habitat_complexity_measures.R
+++ habitat_complexity_measures.R
@@ -6,89 +6,93 @@ library(ggplot2)
 ########## terrain fucntion returns slope, aspect, curvature #########
 terrain_fun <- function(data, cell_size) {
   d0 <- as.matrix(data)
-  da <-  cbind(matrix(NA, nrow = nrow(d0), ncol = 1), 
-               rbind(matrix(NA, nrow = 1, ncol = ncol(d0) - 1), 
-                     matrix(d0[1 : (nrow(d0) - 1), 1 : (ncol(d0) - 1)], nrow = nrow(d0) - 1, ncol = ncol(d0) - 1)))
-  db <- rbind(matrix(NA, nrow = 1, ncol = ncol(d0)), 
-              matrix(d0[1 : (nrow(d0) - 1), ], nrow = nrow(d0) - 1, ncol = ncol(d0)))
-  dc <- cbind(rbind(matrix(NA, nrow = 1, ncol = ncol(d0) - 1), 
-                    matrix(d0[1 : (nrow(d0) - 1), 2 : ncol(d0)], nrow = nrow(d0) - 1, ncol = ncol(d0) - 1)),
-              matrix(NA, nrow = nrow(d0), ncol = 1))
-  dd <- cbind(matrix(NA, nrow = nrow(d0), ncol = 1), 
-              matrix(d0[, 1 : (ncol(d0) - 1)], nrow = nrow(d0), ncol = ncol(d0) -1))
-  df <- cbind(matrix(d0[, 2 : ncol(d0)], nrow = nrow(d0), ncol = ncol(d0) - 1), 
-              matrix(NA, nrow = nrow(d0), ncol = 1))
-  dg <- cbind(matrix(NA, nrow = nrow(d0), ncol = 1), 
-              rbind(matrix(d0[2 : nrow(d0), 1 : (ncol(d0) - 1)], nrow = nrow(d0) - 1, ncol = ncol(d0) - 1), 
-                    matrix(NA, nrow = 1, ncol = ncol(d0) - 1)))
-  dh <- rbind(matrix(d0[2 : nrow(d0), ], nrow = nrow(d0) - 1, ncol = ncol(d0)), 
-              matrix(NA, nrow = 1, ncol = ncol(d0)))
-  di <- cbind(rbind(matrix(d0[2 : nrow(d0), 2 : ncol(d0)], nrow = nrow(d0) - 1, ncol = ncol(d0) - 1), 
-                    matrix(NA, nrow = 1, ncol = (ncol(d0) - 1))), 
-              matrix(NA, nrow = nrow(d0), ncol = 1))
-  
-  ## slope
-  
-  x_rate <- ((dc + (2 * df) + di) - (da + (2 * dd) + dg)) / (8 * cell_size)
-  y_rate <- ((dg + (2 * dh) + di) - (da + (2 * db) + dc)) / (8 * cell_size)
-  
-  slope_degrees <- atan(sqrt(x_rate ^ 2 + y_rate ^ 2)) * 57.29578
-  mean_slope <- mean(slope_degrees, na.rm = TRUE)
-  
-  ## aspect
-  
-  x_rate_aspect <- ((dc + (2 * df) + di) - (da + (2 * dd) + dg)) / 8
-  y_rate_aspect <- ((dg + (2 * dh) + di) - (da + (2 * db) + dc)) / 8
-  
-  aspect <- 57.29578 * atan2(y_rate_aspect, (-1 * x_rate_aspect))
-  
-  aspect_conversion <- function(x) {
-    if (is.na(x)) {
-      cell <- NA
-    } else if (x < 90.0) {
-      cell <- 90.0 - x
-    } else {
-      cell <- 450 - x
+    if (ncol(d0) == 1 | nrow(d0) == 1) {
+    terrain_list <- list(NA, NA, NA, NA, NA, NA)
+    names(terrain_list) <- c("mean_slope", "mean_aspect", "circular_mean_aspect", 
+                             "mean_curvature", "mean_profile_curvature", "mean_plan_curvature")
+  } else {
+    da <-  cbind(matrix(NA, nrow = nrow(d0), ncol = 1), 
+                 rbind(matrix(NA, nrow = 1, ncol = ncol(d0) - 1), 
+                       matrix(d0[1 : (nrow(d0) - 1), 1 : (ncol(d0) - 1)], nrow = nrow(d0) - 1, ncol = ncol(d0) - 1)))
+    db <- rbind(matrix(NA, nrow = 1, ncol = ncol(d0)), 
+                matrix(d0[1 : (nrow(d0) - 1), ], nrow = nrow(d0) - 1, ncol = ncol(d0)))
+    dc <- cbind(rbind(matrix(NA, nrow = 1, ncol = ncol(d0) - 1), 
+                      matrix(d0[1 : (nrow(d0) - 1), 2 : ncol(d0)], nrow = nrow(d0) - 1, ncol = ncol(d0) - 1)),
+                matrix(NA, nrow = nrow(d0), ncol = 1))
+    dd <- cbind(matrix(NA, nrow = nrow(d0), ncol = 1), 
+                matrix(d0[, 1 : (ncol(d0) - 1)], nrow = nrow(d0), ncol = ncol(d0) -1))
+    df <- cbind(matrix(d0[, 2 : ncol(d0)], nrow = nrow(d0), ncol = ncol(d0) - 1), 
+                matrix(NA, nrow = nrow(d0), ncol = 1))
+    dg <- cbind(matrix(NA, nrow = nrow(d0), ncol = 1), 
+                rbind(matrix(d0[2 : nrow(d0), 1 : (ncol(d0) - 1)], nrow = nrow(d0) - 1, ncol = ncol(d0) - 1), 
+                      matrix(NA, nrow = 1, ncol = ncol(d0) - 1)))
+    dh <- rbind(matrix(d0[2 : nrow(d0), ], nrow = nrow(d0) - 1, ncol = ncol(d0)), 
+                matrix(NA, nrow = 1, ncol = ncol(d0)))
+    di <- cbind(rbind(matrix(d0[2 : nrow(d0), 2 : ncol(d0)], nrow = nrow(d0) - 1, ncol = ncol(d0) - 1), 
+                      matrix(NA, nrow = 1, ncol = (ncol(d0) - 1))), 
+                matrix(NA, nrow = nrow(d0), ncol = 1))
+    
+    ## slope
+    
+    x_rate <- ((dc + (2 * df) + di) - (da + (2 * dd) + dg)) / (8 * cell_size)
+    y_rate <- ((dg + (2 * dh) + di) - (da + (2 * db) + dc)) / (8 * cell_size)
+    
+    slope_degrees <- atan(sqrt(x_rate ^ 2 + y_rate ^ 2)) * 57.29578
+    mean_slope <- mean(slope_degrees, na.rm = TRUE)
+    
+    ## aspect
+    
+    x_rate_aspect <- ((dc + (2 * df) + di) - (da + (2 * dd) + dg)) / 8
+    y_rate_aspect <- ((dg + (2 * dh) + di) - (da + (2 * db) + dc)) / 8
+    
+    aspect <- 57.29578 * atan2(y_rate_aspect, (-1 * x_rate_aspect))
+    
+    aspect_conversion <- function(x) {
+      if (is.na(x)) {
+        cell <- NA
+      } else if (x < 90.0) {
+        cell <- 90.0 - x
+      } else {
+        cell <- 450 - x
+      }
+      return(cell)
     }
-    return(cell)
+    
+    aspect_conv <- apply(aspect, c(1, 2), aspect_conversion)
+    mean_aspect <- mean(aspect_conv, na.rm = TRUE)
+    
+    # convert aspect_conv to radians
+    aspect_rad <- aspect_conv * (pi / 180)
+    mean_sin <- mean(sin(aspect_rad), na.rm = TRUE)
+    mean_cos <- mean(cos(aspect_rad), na.rm = TRUE)
+    mean_aspect_degree <- 57.29578 * atan2(mean_sin, mean_cos)
+    mean_aspect_2 <- mean_aspect_degree %% 360
+    
+    ## curvature
+    
+    coefD <- ((dd + df) / 2 - d0) / (cell_size ^ 2)
+    coefE <- ((db + dh) / 2 - d0) / (cell_size ^ 2)
+    coefF <- (dc + dg -da - di) / (4 * (cell_size ^ 2))
+    coefG = (df - dd) / 2 * cell_size
+    coefH = (db - dh) / 2 * cell_size
+    
+    curvature <- 2 * (coefD + coefE)
+    mean_curvature <- mean(curvature, na.rm = TRUE)
+    
+    prof_curv <- -2 * ((coefD * coefG ^ 2 + coefE * coefH ^ 2 + coefF * coefG * coefH) / 
+                         (coefG ^ 2 + coefH ^ 2))
+    mean_prof_curv <- mean(prof_curv, na.rm = TRUE)
+    
+    plan_curv <- 2 * ((coefD * coefH ^ 2 + coefE * coefG ^ 2 - coefF * coefG * coefH) / 
+                        (coefG ^ 2 + coefH ^ 2))
+    mean_plan_curv <- mean(plan_curv, na.rm = TRUE)
+      
+    terrain_list <- list(mean_slope, mean_aspect, mean_aspect_2, mean_curvature, 
+                         mean_prof_curv, mean_plan_curv)
+    names(terrain_list) <- c("mean_slope", "mean_aspect", "circular_mean_aspect", 
+                             "mean_curvature", "mean_profile_curvature", "mean_plan_curvature")
   }
   
-  aspect_conv <- apply(aspect, c(1, 2), aspect_conversion)
-  mean_aspect <- mean(aspect_conv, na.rm = TRUE)
-  
-  # convert aspect_conv to radians
-  aspect_rad <- aspect_conv * (pi / 180)
-  mean_sin <- mean(sin(aspect_rad), na.rm = TRUE)
-  mean_cos <- mean(cos(aspect_rad), na.rm = TRUE)
-  mean_aspect_degree <- 57.29578 * atan2(mean_sin, mean_cos)
-  mean_aspect_2 <- mean_aspect_degree %% 360
-  
-  ## curvature
-  
-  coefD <- ((dd + df) / 2 - d0) / (cell_size ^ 2)
-  coefE <- ((db + dh) / 2 - d0) / (cell_size ^ 2)
-  coefF <- (dc + dg -da - di) / (4 * (cell_size ^ 2))
-  coefG = (df - dd) / 2 * cell_size
-  coefH = (db - dh) / 2 * cell_size
-  
-  curvature <- 2 * (coefD + coefE)
-  mean_curvature <- mean(curvature, na.rm = TRUE)
-  
-  prof_curv <- -2 * ((coefD * coefG ^ 2 + coefE * coefH ^ 2 + coefF * coefG * coefH) / 
-                       (coefG ^ 2 + coefH ^ 2))
-  mean_prof_curv <- mean(prof_curv, na.rm = TRUE)
-  
-  plan_curv <- 2 * ((coefD * coefH ^ 2 + coefE * coefG ^ 2 - coefF * coefG * coefH) / 
-                      (coefG ^ 2 + coefH ^ 2))
-  mean_plan_curv <- mean(plan_curv, na.rm = TRUE)
-  
-  
-  ## create a list
-  
-  terrain_list <- list(mean_slope, mean_aspect, mean_aspect_2, mean_curvature, 
-                       mean_prof_curv, mean_plan_curv)
-  names(terrain_list) <- c("mean_slope", "mean_aspect", "circular_mean_aspect", 
-                           "mean_curvature", "mean_profile_curvature", "mean_plan_curvature")
   return(terrain_list)
   
 }
