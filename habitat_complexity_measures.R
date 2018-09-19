d28e65e866a5181a6ca53057aa5c2f4926edadb1
AtsukoFukunaga <atsuko.fukunaga@hawaii.edu>
2018-09-18 16:03:40 -1000

    updating curvature

--- habitat_complexity_measures.R
+++ habitat_complexity_measures.R
@@ -6,85 +6,93 @@ library(ggplot2)
 ########## terrain fucntion returns slope, aspect, curvature #########
 terrain_fun <- function(data, cell_size) {
   d0 <- as.matrix(data)
-  da <-  cbind(matrix(NA, nrow = nrow(d0), ncol = 1), 
-               rbind(matrix(NA, nrow = 1, ncol = ncol(d0) - 1), 
-                     d0[1 : (nrow(d0) - 1), 1 : (ncol(d0) - 1)]))
-  db <- rbind(matrix(NA, nrow = 1, ncol = ncol(d0)), d0[1 : (nrow(d0) - 1), ])
-  dc <- cbind(rbind(matrix(NA, nrow = 1, ncol = ncol(d0) - 1), 
-                    d0[1 : (nrow(d0) - 1), 2 : ncol(d0)]),
-              matrix(NA, nrow = nrow(d0), ncol = 1))
-  dd <- cbind(matrix(NA, nrow = nrow(d0), ncol = 1), d0[, 1 : (ncol(d0) - 1)])
-  df <- cbind(d0[, 2 : ncol(d0)], matrix(NA, nrow = nrow(d0), ncol = 1))
-  dg <- cbind(matrix(NA, nrow = nrow(d0), ncol = 1), 
-              rbind(d0[2 : nrow(d0), 1 : (ncol(d0) - 1)], 
-                    matrix(NA, nrow = 1, ncol = ncol(d0) - 1)))
-  dh <- rbind(d0[2 : nrow(d0), ], matrix(NA, nrow = 1, ncol = ncol(d0)))
-  di <- cbind(rbind(d0[2 : nrow(d0), 2 : ncol(d0)], 
-                    matrix(NA, nrow = 1, ncol = (ncol(d0) - 1))), 
-              matrix(NA, nrow = nrow(d0), ncol = 1))
-  
-  ## slope
-  
-  x_rate <- ((dc + (2 * df) + di) - (da + (2 * dd) + dg)) / (8 * cell_size)
-  y_rate <- ((dg + (2 * dh) + di) - (da + (2 * db) + dc)) / (8 * cell_size)
-  
-  slope_degrees <- atan(sqrt(x_rate ^ 2 + y_rate ^ 2)) * 57.29578
-  mean_slope <- mean(slope_degrees, na.rm = TRUE)
-  
-  ## aspect
-  
-  x_rate_aspect <- ((dc + (2 * df) + di) - (da + (2 * dd) + dg)) / 8
-  y_rate_aspect <- ((dg + (2 * dh) + di) - (da + (2 * db) + dc)) / 8
-  
-  aspect <- 57.29578 * atan2(y_rate_aspect, (-1 * x_rate_aspect))
-  
-  aspect_conversion <- function(x) {
-    if (is.na(x)) {
-      cell <- NA
-    } else if (x < 90.0) {
-      cell <- 90.0 - x
-    } else {
-      cell <- 450 - x
+  if (ncol(d0) == 1 | nrow(d0) == 1) {
+    terrain_list <- list(NA, NA, NA, NA, NA, NA)
+    names(terrain_list) <- c("mean_slope", "mean_aspect", "circular_mean_aspect", 
+                             "mean_curvature", "mean_profile_curvature", "mean_plan_curvature")
+  } else {
+    da <-  cbind(matrix(NA, nrow = nrow(d0), ncol = 1), 
+                 rbind(matrix(NA, nrow = 1, ncol = ncol(d0) - 1), 
+                       matrix(d0[1 : (nrow(d0) - 1), 1 : (ncol(d0) - 1)], nrow = nrow(d0) - 1, ncol = ncol(d0) - 1)))
+    db <- rbind(matrix(NA, nrow = 1, ncol = ncol(d0)), 
+                matrix(d0[1 : (nrow(d0) - 1), ], nrow = nrow(d0) - 1, ncol = ncol(d0)))
+    dc <- cbind(rbind(matrix(NA, nrow = 1, ncol = ncol(d0) - 1), 
+                      matrix(d0[1 : (nrow(d0) - 1), 2 : ncol(d0)], nrow = nrow(d0) - 1, ncol = ncol(d0) - 1)),
+                matrix(NA, nrow = nrow(d0), ncol = 1))
+    dd <- cbind(matrix(NA, nrow = nrow(d0), ncol = 1), 
+                matrix(d0[, 1 : (ncol(d0) - 1)], nrow = nrow(d0), ncol = ncol(d0) -1))
+    df <- cbind(matrix(d0[, 2 : ncol(d0)], nrow = nrow(d0), ncol = ncol(d0) - 1), 
+                matrix(NA, nrow = nrow(d0), ncol = 1))
+    dg <- cbind(matrix(NA, nrow = nrow(d0), ncol = 1), 
+                rbind(matrix(d0[2 : nrow(d0), 1 : (ncol(d0) - 1)], nrow = nrow(d0) - 1, ncol = ncol(d0) - 1), 
+                      matrix(NA, nrow = 1, ncol = ncol(d0) - 1)))
+    dh <- rbind(matrix(d0[2 : nrow(d0), ], nrow = nrow(d0) - 1, ncol = ncol(d0)), 
+                matrix(NA, nrow = 1, ncol = ncol(d0)))
+    di <- cbind(rbind(matrix(d0[2 : nrow(d0), 2 : ncol(d0)], nrow = nrow(d0) - 1, ncol = ncol(d0) - 1), 
+                      matrix(NA, nrow = 1, ncol = (ncol(d0) - 1))), 
+                matrix(NA, nrow = nrow(d0), ncol = 1))
+    
+    ## slope
+    
+    x_rate <- ((dc + (2 * df) + di) - (da + (2 * dd) + dg)) / (8 * cell_size)
+    y_rate <- ((dg + (2 * dh) + di) - (da + (2 * db) + dc)) / (8 * cell_size)
+    
+    slope_degrees <- atan(sqrt(x_rate ^ 2 + y_rate ^ 2)) * 57.29578
+    mean_slope <- mean(slope_degrees, na.rm = TRUE)
+    
+    ## aspect
+    
+    x_rate_aspect <- ((dc + (2 * df) + di) - (da + (2 * dd) + dg)) / 8
+    y_rate_aspect <- ((dg + (2 * dh) + di) - (da + (2 * db) + dc)) / 8
+    
+    aspect <- 57.29578 * atan2(y_rate_aspect, (-1 * x_rate_aspect))
+    
+    aspect_conversion <- function(x) {
+      if (is.na(x)) {
+        cell <- NA
+      } else if (x < 90.0) {
+        cell <- 90.0 - x
+      } else {
+        cell <- 450 - x
+      }
+      return(cell)
     }
-    return(cell)
+    
+    aspect_conv <- apply(aspect, c(1, 2), aspect_conversion)
+    mean_aspect <- mean(aspect_conv, na.rm = TRUE)
+    
+    # convert aspect_conv to radians
+    aspect_rad <- aspect_conv * (pi / 180)
+    mean_sin <- mean(sin(aspect_rad), na.rm = TRUE)
+    mean_cos <- mean(cos(aspect_rad), na.rm = TRUE)
+    mean_aspect_degree <- 57.29578 * atan2(mean_sin, mean_cos)
+    mean_aspect_2 <- mean_aspect_degree %% 360
+    
+    ## curvature
+    
+    coefD <- (((dd + df) / 2) - d0) / (cell_size ^ 2)
+    coefE <- (((db + dh) / 2) - d0) / (cell_size ^ 2)
+    coefF <- (dc + dg -da - di) / (4 * (cell_size ^ 2))
+    coefG = (df - dd) / 2 * cell_size
+    coefH = (db - dh) / 2 * cell_size
+    
+    curvature <- -2 * (coefD + coefE)
+    mean_curvature <- mean(curvature, na.rm = TRUE)
+    
+    prof_curv <- 2 * ((coefD * coefG ^ 2 + coefE * coefH ^ 2 + coefF * coefG * coefH) / 
+                         (coefG ^ 2 + coefH ^ 2))
+    mean_prof_curv <- mean(prof_curv, na.rm = TRUE)
+    
+    plan_curv <- -2 * ((coefD * coefH ^ 2 + coefE * coefG ^ 2 - coefF * coefG * coefH) / 
+                        (coefG ^ 2 + coefH ^ 2))
+    mean_plan_curv <- mean(plan_curv, na.rm = TRUE)
+    
+    terrain_list <- list(mean_slope, mean_aspect, mean_aspect_2, mean_curvature, 
+                         mean_prof_curv, mean_plan_curv)
+    names(terrain_list) <- c("mean_slope", "mean_aspect", "circular_mean_aspect", 
+                             "mean_curvature", "mean_profile_curvature", "mean_plan_curvature")
   }
   
-  aspect_conv <- apply(aspect, c(1, 2), aspect_conversion)
-  mean_aspect <- mean(aspect_conv, na.rm = TRUE)
-  
-  # convert aspect_conv to radians
-  aspect_rad <- aspect_conv * (pi / 180)
-  mean_sin <- mean(sin(aspect_rad), na.rm = TRUE)
-  mean_cos <- mean(cos(aspect_rad), na.rm = TRUE)
-  mean_aspect_degree <- 57.29578 * atan2(mean_sin, mean_cos)
-  mean_aspect_2 <- mean_aspect_degree %% 360
-  
-  ## curvature
-  
-  coefD <- ((dd + df) / 2 - d0) / (cell_size ^ 2)
-  coefE <- ((db + dh) / 2 - d0) / (cell_size ^ 2)
-  coefF <- (dc + dg -da - di) / (4 * (cell_size ^ 2))
-  coefG = (df - dd) / 2 * cell_size
-  coefH = (db - dh) / 2 * cell_size
-  
-  curvature <- 2 * (coefD + coefE)
-  mean_curvature <- mean(curvature, na.rm = TRUE)
-  
-  prof_curv <- -2 * ((coefD * coefG ^ 2 + coefE * coefH ^ 2 + coefF * coefG * coefH) / 
-                       (coefG ^ 2 + coefH ^ 2))
-  mean_prof_curv <- mean(prof_curv, na.rm = TRUE)
-  
-  plan_curv <- 2 * ((coefD * coefH ^ 2 + coefE * coefG ^ 2 - coefF * coefG * coefH) / 
-                      (coefG ^ 2 + coefH ^ 2))
-  mean_plan_curv <- mean(plan_curv, na.rm = TRUE)
-  
-  
-  ## create a list
-  
-  terrain_list <- list(mean_slope, mean_aspect, mean_aspect_2, mean_curvature, 
-                       mean_prof_curv, mean_plan_curv)
-  names(terrain_list) <- c("mean_slope", "mean_aspect", "circular_mean_aspect", 
-                           "mean_curvature", "mean_profile_curvature", "mean_plan_curvature")
   return(terrain_list)
   
 }
@@ -92,13 +100,14 @@ terrain_fun <- function(data, cell_size) {
 
 ########## process files #########
 
-file_path <- ""
-file_names <- c("FFS_4185")
-file_suf <- "_DEM_1cm.tif"
+file_path <- "~/Habitat_complexity_test/DEMs/"
+file_names <- c("LIS_4254")
+file_suf <- "_1_1.tif"
 
 files <- paste(file_path, file_names, file_suf, sep = "")
 resolution <- 0.01  # DEM resolution = 1cm
-                
+return_resolution_factor <- 1  # resolution factor for return values, use 1, 2, 4, 8, 16, 32, 64. 1 for 1 cm, 2 for 2 cm etc.
+
 hab_data <- data.frame(file_name = character(0), fd64 = numeric(0), fd128 = numeric(0),
                        planerS64 = numeric(0), planerS128 = numeric(0),
                        rugosity = numeric(0), max_h = numeric(0), min_h = numeric(0),
@@ -111,6 +120,8 @@ hab_data <- data.frame(file_name = character(0), fd64 = numeric(0), fd128 = nume
 
 for (k in 1:length(files)) {
   ras <- raster(files[k])
+  
+  print(file_names[k])
   
   extent64 <- aggregate(ras, fac = 64, fun = mean, expand = FALSE, na.rm = FALSE)
   extent64_uniform <- extent64 > -Inf
@@ -136,9 +147,12 @@ for (k in 1:length(files)) {
   
   
   for (j in 1:length(dat)) {
-  
+    
     if (j == 1) {
       for (i in 1:nrow(dat[[j]])) {
+        
+        print(dat[[j]]$fac[i])
+        
         if (dat[[j]]$fac[i] == 1) {
           reef <- ras_clip_64
         } else {
@@ -160,6 +174,9 @@ for (k in 1:length(files)) {
       }
     } else if (j == 2) {
       for (i in 1:nrow(dat[[j]])) {
+        
+        print(dat[[j]]$fac[i])
+        
         if (dat[[j]]$fac[i] == 1) {
           reef <- ras_clip_128
         } else {
@@ -185,14 +202,26 @@ for (k in 1:length(files)) {
   
   p64 <- ggplot(dat[[1]], aes(x = log(fac * 0.01), y = log(s_area))) +
     geom_point() +
-    geom_smooth(method = "lm", se = F)
+    geom_smooth(method = "lm", se = F) +
+    xlim(c(-5, 0)) + ylim(c(4.4, 6.5)) + 
+    labs(x = expression(paste("log(", delta, ")")),
+         y = expression(paste("logS(", delta, ")")))
   
   p128 <- ggplot(dat[[2]], aes(x = log(fac * 0.01), y = log(s_area))) +
     geom_point() +
-    geom_smooth(method = "lm", se = F)
+    geom_smooth(method = "lm", se = F) + 
+    xlim(c(-5, 1)) + ylim(c(4.4, 6.5)) + 
+    labs(x = expression(paste("log(", delta, ")")),
+         y = expression(paste("logS(", delta, ")"))) +
+    theme_bw()
   
-  plot(p64)
-  plot(p128)
+  # png("FD_plot", height = 1.2, width = 1.2, units = 'in', 
+  #     type = "windows", res = 300)
+  # p128
+  # dev.off()
+  
+  #plot(p64)
+  #plot(p128)
   
   d64 <- lm(log(s_area/area) ~ log(fac * 0.01), data = dat[[1]])
   slope64 <- coef(d64)[[2]]
@@ -202,23 +231,25 @@ for (k in 1:length(files)) {
   slope128 <- coef(d128)[[2]]
   fd128 <- 2 - slope128
   
-  rugosity64 <- dat[[1]]$s_area[1]/dat[[1]]$area[1]
+  fac <- which(dat[[1]]$fac == return_resolution_factor)
   
-  planerS64 <- dat[[1]]$area[1]
-  planerS128 <- dat[[2]]$area[1]
+  rugosity64 <- dat[[1]]$s_area[fac]/dat[[1]]$area[fac]
   
-  max_h <- dat[[1]]$max_height[1]
-  min_h <- dat[[1]]$min_height[1]
+  planerS64 <- dat[[1]]$area[fac]
+  planerS128 <- dat[[2]]$area[fac]
+  
+  max_h <- dat[[1]]$max_height[fac]
+  min_h <- dat[[1]]$min_height[fac]
   diff_h <- max_h - min_h
-  sd_h <- dat[[1]]$height_std[1]
+  sd_h <- dat[[1]]$height_std[fac]
   
-  mean_slope <- dat[[1]]$mean_slope[1]
-  mean_aspect <- dat[[1]]$mean_aspect[1]
-  circular_mean_aspect <- dat[[1]]$circular_mean_aspect[1]
-  mean_curvature <- dat[[1]]$mean_curvature[1]
-  mean_profile_curvature <- dat[[1]]$mean_profile_curvature[1]
-  mean_plan_curvature <- dat[[1]]$mean_plan_curvature[1]
- 
+  mean_slope <- dat[[1]]$mean_slope[fac]
+  mean_aspect <- dat[[1]]$mean_aspect[fac]
+  circular_mean_aspect <- dat[[1]]$circular_mean_aspect[fac]
+  mean_curvature <- dat[[1]]$mean_curvature[fac]
+  mean_profile_curvature <- dat[[1]]$mean_profile_curvature[fac]
+  mean_plan_curvature <- dat[[1]]$mean_plan_curvature[fac]
+  
   temp <- data.frame(file_name = file_names[k], fd64 = fd64, fd128 = fd128,
                      planerS64 = planerS64, planerS128 = planerS128,
                      rugosity = rugosity64, max_h = max_h, min_h = min_h,
